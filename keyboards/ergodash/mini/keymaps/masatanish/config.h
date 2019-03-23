cf2135316240991120d46e5219a3db6462b936d3
masata <masatanish@gmail.com>
2019-03-23 14:52:33 +0900

    modify keymap

--- keyboards/ergodash/mini/keymaps/masatanish/config.h
+++ keyboards/ergodash/mini/keymaps/masatanish/config.h
@@ -37,3 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+
+#define RETRO_TAPPING
+#define PERMISSIVE_HOLD
